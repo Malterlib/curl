5ffb2eeaad004ae606fafb5224912e9472661fde
Erik Olofsson <erik.olofsson@favro.com>
2018-11-21 07:24:45 +0100

    Add support for external ca store fallback

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -228,6 +228,10 @@ struct ssl_backend_data {
 #endif
 };
 
+#ifdef CURL_CA_EXTERNAL_FALLBACK
+void curl_ca_external_fallback(X509_STORE *sslstore);
+#endif
+
 /*
  * Number of bytes to read from the random number seed file. This must be
  * a finite value (because some entropy "files" like /dev/urandom have
@@ -2776,7 +2780,11 @@ static CURLcode ossl_connect_step1(struct connectdata *conn, int sockindex)
   }
 #endif
 
-#ifdef CURL_CA_FALLBACK
+#ifdef CURL_CA_EXTERNAL_FALLBACK
+  else {
+    curl_ca_external_fallback(SSL_CTX_get_cert_store(backend->ctx));
+  }
+#elif defined(CURL_CA_FALLBACK)
   else if(verifypeer) {
     /* verifying the peer without any CA certificates won't
        work so use openssl's built in default as fallback */
