49e0ad02c749fa738beae136a73383235d04cebf
Erik Olofsson <erik.olofsson@favro.com>
2018-11-21 07:25:43 +0100

    Fix Windows compile issues

--- lib/easy.c
+++ lib/easy.c
@@ -93,7 +93,7 @@ static long          init_flags;
  * ways, but at this point it must be defined as the system-supplied strdup
  * so the callback pointer is initialized correctly.
  */
-#if defined(_WIN32_WCE)
+#if defined(WIN32)
 #define system_strdup _strdup
 #elif !defined(HAVE_STRDUP)
 #define system_strdup curlx_strdup
