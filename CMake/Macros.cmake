b311321cdfe932904d3912038d9f0ac7aeedf50d
Erik Olofsson <erik@olofsson.info>
2026-09-09 20:24:07 +0200

    cmake: preserve library lists in feature probes
    Pass required libraries as separate try_compile link arguments so imported thread targets are recognized.

--- CMake/Macros.cmake
+++ CMake/Macros.cmake
@@ -45,7 +45,7 @@ macro(curl_internal_test _curl_test)
       ${PROJECT_BINARY_DIR}
       "${PROJECT_SOURCE_DIR}/CMake/CurlTests.c"
       COMPILE_DEFINITIONS "-D${_curl_test}" ${CURL_TEST_DEFINES} ${CMAKE_REQUIRED_FLAGS} ${CMAKE_REQUIRED_DEFINITIONS}
-      LINK_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES}"
+      LINK_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES}
       OUTPUT_VARIABLE CURL_TEST_OUTPUT)
     if(${_curl_test})
       set(${_curl_test} 1 CACHE INTERNAL "curl test")
